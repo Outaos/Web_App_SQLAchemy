b8d4f4704f2357b9281b607f185ad86e4c85576a
Outaos <outao_eu@yahoo.com>
2022-08-13 18:50:02 +0100

    Redesigned the interface. Added "Play" button

--- main.py
+++ main.py
@@ -28,6 +28,10 @@ class Upload(db.Model):
 def home():
     return render_template('./home.html')
 
+@app.route('/play')
+def play():
+    return render_template('./play.html')
+
 @app.route('/art')
 def art():
     return render_template('./art.html')
